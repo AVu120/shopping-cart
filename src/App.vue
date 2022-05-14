44f8659f42abdfe0af364b21e3493fcba15e1c62
Anthony Hien Vu <anthony.vu@checkbox.ai>
2022-05-14 17:37:49 +1000

    Add template for shopping cart demo.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="wrapper">Socks</div>
+  <div class="container">
+    <img class="product_image" src="/socks_green.jpeg" />
+    <div class="product_info">
+      <h1>Socks</h1>
+      <p>In Stock</p>
+    </div>
+  </div>
 </template>
 
 <style>
 @import "@/assets/base.css";
 
-.wrapper {
-  height: 100vh;
+.container {
   display: flex;
   justify-content: center;
-  align-items: center;
+  height: 100vh;
+  padding-top: 5rem;
+}
+
+.product_image {
+  height: 300px;
+  width: 300px;
+}
+
+.product_info {
+  display: flex;
+  flex-direction: column;
 }
 </style>
